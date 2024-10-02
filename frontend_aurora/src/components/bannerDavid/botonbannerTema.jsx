36e309cc7e1b9743919daa47af583f421bd7dd5e
DavidRamirez152 <davidf_ramirezm@soy.sena.edu.co>
2024-10-02 18:58:39 -0500

    Styles-DarkTheme
    Actualización estilo de tablas y agregación de tema oscuro

--- frontend_aurora/src/components/bannerDavid/botonbannerTema.jsx
+++ frontend_aurora/src/components/bannerDavid/botonbannerTema.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const BotonBannerTema = ({icon}) => {
+    const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        document.documentElement.setAttribute('data-theme', newTheme);
+    };
+
     return (
-        <button className="optionContainerBtn">
+        <button className="optionContainerBtn" onClick={toggleTheme}>
             <div className="optionContainerBtnOne">
                 <span className="material-symbols-outlined">{icon}</span>
                 <div></div>
